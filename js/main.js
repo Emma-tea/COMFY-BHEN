e91004529c3e05e05b5f7395a939b7777092465e
Emma-tea <janettesty4@gmail.com>
2025-03-24 07:07:55 -0700

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,36 @@
+AOS.init();
+
+// You can also pass an optional settings object
+// below listed default settings
+AOS.init({
+  
+  
+
+  // Settings that can be overridden on per-element basis, by `data-aos-*` attributes:
+  offset: 120, // offset (in px) from the original trigger point
+  delay: 0, // values from 0 to 3000, with step 50ms
+  duration: 1500, // values from 0 to 3000, with step 50ms
+  easing: 'ease', // default easing for AOS animations
+  once: false, // whether animation should happen only once - while scrolling down
+  mirror: false, // whether elements should animate out while scrolling past them
+  anchorPlacement: 'top-bottom', // defines which position of the element regarding to window should trigger the animation
+
+});
+// 
+  //  var slideIndex = 0;
+// carousel();
+
+//  function carousel() {
+  // var i;
+  // var x = document.getElementsByClassName("mySlides");
+  // for (i = 0; i < x.length; i++) {
+    // x[i].style.display = "none";
+  // }
+  // slideIndex++;
+  // if (slideIndex > x.length) {slideIndex = 1}
+  // x[slideIndex-1].style.display = "block";
+  // setTimeout(carousel, 2000); // Change image every 2 seconds
+    //  }
+ 
+
+
